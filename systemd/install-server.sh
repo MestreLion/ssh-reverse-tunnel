3aff3377f247a170bdc607628f3c08ddc9c16b24
MestreLion <linux@rodrigosilva.com>
2023-06-04 14:42:32 -0300

    add sshd settings to systemd solution

--- systemd/install-server.sh
+++ systemd/install-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# install-server - SSH Reverse Tunnel server installer
+# install-server - SSH Reverse Tunnel server installer (user and sshd settings)
 #
 # This file is part of <https://github.com/MestreLion/ssh-reverse-tunnel>
 # Copyright (C) 2023 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
@@ -46,6 +46,7 @@ if [[ -z "$key" ]]; then
 	exit 1
 fi
 
+# Create and setup up user
 if ! user_exists "$user"; then sudo adduser "${useropts[@]}" -- "$user"; fi
 home=$(user_home "$user")  # might be different than specified if already existed
 sudo -u "$user" mkdir -pm 0700 -- "${file%/*}"
@@ -53,3 +54,31 @@ if ! [[ -f "$file" ]] || ! grep -Fx -- "$key" "$file"; then
 	sudo -u "$user" tee -a -- "$file" <<< "$key"
 fi
 sudo -u "$user" chmod 0600 -- "$file"
+
+# Tune sshd for reverse tunnels
+sudo tee /etc/ssh/sshd_config.d/00-ssh-reverse-tunnel.conf >/dev/null <<EOF
+# SSH Reverse Tunnel server settings
+# https://github.com/MestreLion/ssh-reverse-tunnel
+#
+# All settings are optional, and limited to the ${user} service account:
+# - Improve stability by disconnecting unresponsive clients to free used ports,
+#    so clients can re-connect and re-create the tunnels.
+# - Improve security by further restricting client permissions, as the connection
+#    is only meant to set up the reverse tunnels.
+# - Improve tunnels' capabilities, allowing to bind ports to external interfaces
+
+Match User ${user}
+	# Disconnect if client is unresponsive for 3 * 20 = 60 seconds.
+	# Time without data to check if client is alive. Default: 0, no check.
+	ClientAliveInterval    20
+	# Attempts for above check to fail before disconnecting. Default: already 3.
+	ClientAliveCountMax     3
+
+	# Security: disable agent, restrict to pubkey, disable interactive sessions
+	AllowAgentForwarding   no
+	AuthenticationMethods  publickey
+	PermitTTY              no
+
+	# Allow binding tunnels to all interfaces instead of localhost only
+	GatewayPorts           yes
+EOF
